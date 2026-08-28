f736b8f28328b557223ff83baa871bd9b3ffa940
kcpbabulu <bankaltimtara.kcpbabulu@gmail.com>
2026-08-28 10:00:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -399,7 +399,7 @@ async function processEJ() {
 
     // [PERBAIKAN 1]: Kamus Pemetaan ID Mesin (Bisa ditambah jika ada kasus serupa)
     const atmIdMap = {
-        "346": "KTM12902"
+        "346": "KTM12902",
         "075": "KTM12901"
     };
 
